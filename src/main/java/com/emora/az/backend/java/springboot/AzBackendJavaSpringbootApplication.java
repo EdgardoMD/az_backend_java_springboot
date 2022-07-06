b5cabc7e34cd8ff2a946cb86bcdadc91ab0d9559
Edgardo Mora <edgardomora@hotmail.com>
2022-07-06 18:24:24 -0400

    commit inicial

---
+++ src/main/java/com/emora/az/backend/java/springboot/AzBackendJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.emora.az.backend.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzBackendJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzBackendJavaSpringbootApplication.class, args);
+	}
+
+}
