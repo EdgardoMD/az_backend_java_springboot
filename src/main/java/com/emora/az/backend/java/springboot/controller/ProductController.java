774cc67f7c5fbe8133cd595a506dac6fc783fe4f
Edgardo Mora <edgardomora@hotmail.com>
2022-07-07 13:51:59 -0400

    agrega anoración @CrossOrigin en ProductController para trabajar con front angular

--- src/main/java/com/emora/az/backend/java/springboot/controller/ProductController.java
+++ src/main/java/com/emora/az/backend/java/springboot/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.emora.az.backend.java.springboot.model.Product;
 import com.emora.az.backend.java.springboot.service.IProductService;
 
 @RestController
+@CrossOrigin({"http://localhost:4200"})
 @RequestMapping("/products")
 public class ProductController {
 	
